a9dcd387a30ad567ebf8544cc414b057c7cb168d
Charles <ellerkaresz@gmail.com>
2019-10-09 20:01:02 +0200

    create dashboard (desktop)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 const Dashboard = ({ user }) => {
   if (!user.email) return <Redirect to="/" />;
 
   return (
-    <div>
-      dashboard for {user.email}
+    <div className="dashboard">
+      <aside className="sidebar">
+        <Link to="/profile">Profile</Link>
+        <Link to="/settings">Settings</Link>
+        <Link to="/">Logout</Link>
+      </aside>
+      <header className="header">Appointments - Manage your appointments efficiently</header>
+      <main className="content">
+        <section className="todays">
+          <h1>Today's appointments</h1>
+          <article>
+            <h1>Place name</h1>
+            <p>Appointment info</p>
+            <p>{new Date().toLocaleString()}</p>
+            <div className="buttons">
+              <button className="done">Done</button>
+              <button className="missed">Missed</button>
+              <button className="delayed">Delayed</button>
+            </div>
+          </article>
+          <article>
+            <h1>Place name</h1>
+            <p>Appointment info</p>
+            <p>{new Date().toLocaleString()}</p>
+            <div className="buttons">
+              <button className="done">Done</button>
+              <button className="missed">Missed</button>
+              <button className="delayed">Delayed</button>
+            </div>
+          </article>
+        </section>
+        <section>
+          <h1 className="missed">Missed appointments</h1>
+          <article>
+            <h1>Place name</h1>
+            <p>Appointment info</p>
+            <p>{new Date().toLocaleString()}</p>
+            <div className="buttons">
+              <button className="done">Reschedule</button>
+            </div>
+          </article>
+        </section>
+      </main>
     </div>
   )
 }
