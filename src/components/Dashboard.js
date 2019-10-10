16bdcc1efccf1aa2ee0351e3d77d7f958135cf6d
Charles <ellerkaresz@gmail.com>
2019-10-10 18:32:13 +0200

    create dashboard (mobile) & sidebar touch control

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import { MenuIcon } from '../icons';
 
 const Dashboard = ({ user }) => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  useEffect(() => {
+    let startTouch;
+    document.addEventListener('touchstart', e => startTouch = { x: e.touches[0].clientX, y: e.touches[0].clientY });
+    document.addEventListener('touchmove', e => {
+      const movingTouch = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+
+      if (movingTouch.y > startTouch.y - 50 && movingTouch.y < startTouch.y + 50) {
+        if (movingTouch.x > startTouch.x + 100) {
+          setOpenSidebar(true);
+        } else if (movingTouch.x < startTouch.x - 100) {
+          setOpenSidebar(false);
+        }
+      }
+    });
+  }, []);
+
   if (!user.email) return <Redirect to="/" />;
 
   return (
     <div className="dashboard">
-      <aside className="sidebar">
+      <aside className={`sidebar ${openSidebar && 'open'}`}>
         <Link to="/profile">Profile</Link>
         <Link to="/settings">Settings</Link>
         <Link to="/">Logout</Link>
       </aside>
-      <header className="header">Appointments - Manage your appointments efficiently</header>
+      <header className="header">
+        <div className="menuicon-wrapper" onClick={() => setOpenSidebar(!openSidebar)}><MenuIcon /></div>
+        Appointments - Manage your appointments efficiently
+      </header>
       <main className="content">
         <section className="todays">
           <h1>Today's appointments</h1>
