89c52998236386da5b7b7693dd9210961985aa11
Charles <ellerkaresz@gmail.com>
2019-10-12 14:40:28 +0200

    create profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,12 @@ import Dashboard from './Dashboard';
 const Profile = ({ user }) => {
   return (
     <Dashboard user={user}>
-      profile
+      <div className="upper-profile">
+        <img src="https://www.flynz.co.nz/wp-content/uploads/profile-placeholder.png" alt="Profile" />
+        <h1>John Doe</h1>
+      </div>
+      <textarea placeholder="Bio"></textarea>
+      <input type="text" placeholder="Location" />
     </Dashboard>
   )
 }
