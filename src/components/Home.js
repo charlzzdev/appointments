76fd872f9a0dc9ed894cee69987fcb7de0d56c4c
Charles <ellerkaresz@gmail.com>
2019-11-12 15:46:52 +0100

    get appointments from firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import Dashboard from './Dashboard';
 
 const Home = ({ user }) => {
+  const [appointments, setAppointments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    firebase.firestore().collection('/users').doc(user.email).get()
+      .then(snapshot => {
+        setAppointments(snapshot.data().appointments);
+        setLoading(false);
+      });
+  }, [user.email]);
+
   return (
     <Dashboard user={user}>
-      <section className="todays">
-        <h1>Today's appointments</h1>
-        <article>
-          <h1>Place name</h1>
-          <p>Appointment info</p>
-          <p>{new Date().toLocaleString()}</p>
-          <div className="buttons">
-            <button className="done">Done</button>
-            <button className="missed">Missed</button>
-            <button className="delayed">Delayed</button>
-          </div>
-        </article>
-        <article>
-          <h1>Place name</h1>
-          <p>Appointment info</p>
-          <p>{new Date().toLocaleString()}</p>
-          <div className="buttons">
-            <button className="done">Done</button>
-            <button className="missed">Missed</button>
-            <button className="delayed">Delayed</button>
-          </div>
-        </article>
-      </section>
-      <section>
-        <h1 className="missed">Missed appointments</h1>
-        <article>
-          <h1>Place name</h1>
-          <p>Appointment info</p>
-          <p>{new Date().toLocaleString()}</p>
-          <div className="buttons">
-            <button className="done">Reschedule</button>
-          </div>
-        </article>
-      </section>
+      {!loading ? (
+        <>
+          <section className="todays">
+            <h1>Today's appointments</h1>
+            {
+              appointments.filter(a => a.status !== 'missed').map(appointment => (
+                <article key={Math.random()}>
+                  <h1>{appointment.place}</h1>
+                  <p>{appointment.info}</p>
+                  <p>{appointment.date}</p>
+                  <div className="buttons">
+                    <button className="done">Done</button>
+                    <button className="missed">Missed</button>
+                    <button className="delayed">Delayed</button>
+                  </div>
+                </article>
+              ))
+            }
+          </section>
+          <section>
+            <h1 className="missed">Missed appointments</h1>
+            {
+              appointments.filter(a => a.status === 'missed').map(appointment => (
+                <article key={Math.random()}>
+                  <h1>{appointment.place}</h1>
+                  <p>{appointment.info}</p>
+                  <p>{appointment.date}</p>
+                  <div className="buttons">
+                    <button className="done">Reschedule</button>
+                  </div>
+                </article>
+              ))
+            }
+          </section>
+        </>
+      ) : 'Loading'}
     </Dashboard>
   )
 }
